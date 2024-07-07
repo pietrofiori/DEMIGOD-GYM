74006fe725c9031d7d4b7a47fe14ea6162e9c16b
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2024-07-07 18:33:10 -0300

    Relatório finalizado , PROJETO PRONTO , BORA PORRAAAAAAAAAAA

--- src/pages/admin/RelatoriosPage.tsx
+++ src/pages/admin/RelatoriosPage.tsx
@@ -9,6 +9,8 @@ import { useMaquinas } from "@/components/maquinas/MaquinasContext";
 import { useAvaliacoes } from "@/components/avaliacoes/AvaliacoesContext";
 import { useFiliais } from "@/components/filiais/FiliaisContext";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { User, DollarSign, Calendar, Clipboard, Wrench ,CheckSquare, MapPin, Users } from "lucide-react";
 
 const RelatoriosPage = () => {
   const { accounts } = useAccounts();
@@ -20,122 +22,148 @@ const RelatoriosPage = () => {
   const { avaliacoes } = useAvaliacoes();
   const { filiais } = useFiliais();
 
+  const today = new Date().toISOString().split("T")[0];
+
   return (
-    <div className="dashboard-container">
-      <Row gutter={[16, 16]}>
-        <Col xs={24} sm={12} lg={8} xxl={6}>
+    <div className="dashboard-container m-7">
+      <div className="w-full flex mb-7 items-center justify-between">
+        <CardTitle>Relatórios</CardTitle>
+        <Input type="date" value={today} className="bg-card w-[300px]" />
+      </div>
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-1/2 pr-4">
+          <Row gutter={[16, 16]}>
+            <Col xs={24} sm={12} lg={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Contas</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    {accounts.map((account) => (
+                      <li key={account.id} className="flex items-center">
+                        <DollarSign className="mr-2" /> {account.nome}
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} lg={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Instrutores</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    {instrutores.map((instrutor) => (
+                      <li key={instrutor.id} className="flex items-center">
+                        <Users className="mr-2" /> {instrutor.nome}
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} lg={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Planos</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    {planos.map((plano) => (
+                      <li key={plano.id} className="flex items-center">
+                        <Clipboard className="mr-2" /> {plano.nome}
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} lg={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Modalidades</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    {modalidades.map((modalidade) => (
+                      <li key={modalidade.id} className="flex items-center">
+                        <Calendar className="mr-2" /> {modalidade.nome}
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} lg={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Máquinas</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    {maquinas.map((maquina) => (
+                      <li key={maquina.id} className="flex items-center">
+                        <Wrench className="mr-2" /> {maquina.nome}
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} lg={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Avaliações</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    {avaliacoes.map((avaliacao) => (
+                      <li key={avaliacao.id} className="flex items-center">
+                        <CheckSquare className="mr-2" /> {avaliacao.nomeSala}
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </Card>
+            </Col>
+            <Col xs={24} sm={12} lg={12}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Filiais</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    {filiais.map((filial) => (
+                      <li key={filial.id} className="flex items-center">
+                        <MapPin className="mr-2" /> {filial.endereco}
+                      </li>
+                    ))}
+                  </ul>
+                </CardContent>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+        <div className="w-full md:w-1/2 pl-4">
           <Card>
             <CardHeader>
-              <CardTitle>Contas</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ul>
-                {accounts.map((account) => (
-                  <li key={account.id}>{account.nome}</li>
-                ))}
-              </ul>
-            </CardContent>
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={8} xxl={6}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Clientes</CardTitle>
+              <CardTitle>Usuários Ativos</CardTitle>
             </CardHeader>
             <CardContent>
               <ul>
                 {clients.map((client) => (
-                  <li key={client.id}>{client.nome}</li>
+                  <li key={client.id} className="flex items-center">
+                    <User className="mr-2" /> {client.nome}
+                  </li>
                 ))}
               </ul>
             </CardContent>
           </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={8} xxl={6}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Instrutores</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ul>
-                {instrutores.map((instrutor) => (
-                  <li key={instrutor.id}>{instrutor.nome}</li>
-                ))}
-              </ul>
-            </CardContent>
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={8} xxl={6}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Planos</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ul>
-                {planos.map((plano) => (
-                  <li key={plano.id}>{plano.nome}</li>
-                ))}
-              </ul>
-            </CardContent>
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={8} xxl={6}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Modalidades</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ul>
-                {modalidades.map((modalidade) => (
-                  <li key={modalidade.id}>{modalidade.nome}</li>
-                ))}
-              </ul>
-            </CardContent>
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={8} xxl={6}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Máquinas</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ul>
-                {maquinas.map((maquina) => (
-                  <li key={maquina.id}>{maquina.nome}</li>
-                ))}
-              </ul>
-            </CardContent>
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={8} xxl={6}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Avaliações</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ul>
-                {avaliacoes.map((avaliacao) => (
-                  <li key={avaliacao.id}>{avaliacao.nomeSala}</li>
-                ))}
-              </ul>
-            </CardContent>
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={8} xxl={6}>
-          <Card>
-            <CardHeader>
-              <CardTitle>Filiais</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ul>
-                {filiais.map((filial) => (
-                  <li key={filial.id}>{filial.endereco}</li>
-                ))}
-              </ul>
-            </CardContent>
-          </Card>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </div>
   );
 };
